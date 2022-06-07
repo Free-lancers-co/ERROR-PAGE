faa52fbd4f78a7b0a93851bd3a35f22431b19724
Philip-webdev <106782051+Philip-webdev@users.noreply.github.com>
2022-06-07 11:23:11 +0100

    Create Philip.js

---
+++ Philip.js
@@ -0,0 +1,13 @@
+
+
+let txt;
+txt="beans"
+try{
+				if(txt="rice")
+				{throw "err1"}
+				else if(txt="porriadge")
+				{throw "err2"}
+}
+catch(err){
+				if(txt=="err1"){alert("404 error  this page cannot be found");}
+				if(txt=="err2"){alert("404 error  page not found");} {document.getElementById("food")} }
