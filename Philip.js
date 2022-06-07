505ea5f35006019474ed0832fc7d15e32db0b39e
Philip-webdev <106782051+Philip-webdev@users.noreply.github.com>
2022-06-07 11:37:13 +0100

    Update Philip.js

--- Philip.js
+++ Philip.js
@@ -5,9 +5,17 @@ txt="beans"
 try{
 				if(txt="rice")
 				{throw "err1"}
-				else if(txt="porriadge")
+				else if(txt="porridge")
 				{throw "err2"}
 }
 catch(err){
 				if(txt=="err1"){alert("404 error  this page cannot be found");}
 				if(txt=="err2"){alert("404 error  page not found");} {document.getElementById("food")} }
+
+
+//<footer>
+//<ul>
+												//<li class="current"><a href="Home.html">Home</a></li>
+												//<li class="current"><a href="About Us.html">About Us</a></li>												//<li class="current"><a href="Contact Us.html">Contact Us</a></li>
+												//<li class="current"><a href="more.html">More</a></li>
+//<text>Philip's page copyright &copy; 2022</text>
